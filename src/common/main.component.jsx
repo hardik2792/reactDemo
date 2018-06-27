5d35835ac3b16c901f88013825e388a00dafa476
mrugank93 <mrugank_1993@outlook.com>
2018-06-27 18:30:27 +0530

    Route added

--- src/common/main.component.jsx
+++ src/common/main.component.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 
 class Main extends Component {
     render(){
@@ -12,8 +12,9 @@ class Main extends Component {
                         </div>
                         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul className="nav navbar-nav">
-                                <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-                                <li><a href="#">Link</a></li>
+                              <li><Link to="/">Home</Link></li>
+                              <li><Link to="/cars">Cars</Link></li>
+                              <li><Link to="/about">About</Link></li>
                             </ul>
                         </div>
                     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -2,50 +2,46 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 // Import routing components
-// import {HashRouter, Route} from 'react-router-dom';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Switch} from 'react-router';
+// import {HashRouter, Route, IndexRoute, browserHistory} from 'react-router-dom';
+import { BrowserRouter as Router, IndexRoute, Route, Link, browserHistory } from "react-router-dom";
 
 import Main from './common/main.component.jsx'
 import Home from './common/home.component.jsx'
 import About from './common/about.component.jsx'
 import Car from './car/car.component.jsx'
 
-// class Home extends Component {
-//     render(){
-//         return (<h1>Hi</h1>);
-//     }
-// }
-//
-// // More components
-// class Car extends Component {
-//     render(){
-//         return (<h1>Cars page</h1>);
-//     }
-// }
-//
-// class About extends Component {
-//     render(){
-//         return (<h1>About page</h1>);
-//     }
-// }
+ReactDOM.render(
+      <Router>
+          <div>
+              <nav className="navbar navbar-default">
+                  <div className="container-fluid">
+                      <div className="navbar-header">
+                          <a className="navbar-brand" href="#">Scotch Cars</a>
+                      </div>
+                      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                          <ul className="nav navbar-nav">
+                            <li><Link to="/home">Home</Link></li>
+                            <li><Link to="/cars">Cars</Link></li>
+                            <li><Link to="/about">About</Link></li>
+                          </ul>
+                      </div>
+                  </div>
+              </nav>
 
-// ReactDOM.render(
-//     <HashRouter path="/" component={Main}  path="app">
-//       <div>
-//           <Route exact path="/" component={Home}/>
-//           <Route exact path="/cars" component={Car}/>
-//           <Route exact path="/about" component={About}/>
-//       </div>
-//     </HashRouter>,
-//     document.getElementById('container')
-// );
+              <Route path="/home" component={Home}/>
+              <Route path="/cars" component={Car}/>
+              <Route path="/about" component={About}/>
+          </div>
 
-<Router>
-    <div>
-      <Route path="/" component={Main} history={browserHistory}>
-          <IndexRoute component={Home} />
-          <Route path="/cars" component={Car}/>
-          <Route path="/about" component={About}/>
-      </Route>
-    </div>
-</Router>
+      </Router>,
+    document.getElementById('container')
+);
+
+// <Router>
+//       <Route path="/" component={Main} history={browserHistory}>
+//           <IndexRoute component={Home} />
+//           <Route path="/cars" component={Car}/>
+//           <Route path="/about" component={About}/>
+//       </Route>
+// </Router>
