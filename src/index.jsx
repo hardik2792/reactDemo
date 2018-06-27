876df50ad8e91604cd266992727abd514e2c298a
mrugank93 <mrugank_1993@outlook.com>
2018-06-27 15:28:51 +0530

    init

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
 // Import routing components
-import {HashRouter, Route} from 'react-router-dom';
+// import {HashRouter, Route} from 'react-router-dom';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
 import Main from './common/main.component.jsx'
 import Home from './common/home.component.jsx'
 import About from './common/about.component.jsx'
@@ -26,13 +29,23 @@ import Car from './car/car.component.jsx'
 //     }
 // }
 
-ReactDOM.render(
-    <HashRouter component={Main}>
-      <div>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/cars" component={Car}/>
-          <Route exact path="/about" component={About}/>
-      </div>
-    </HashRouter>,
-    document.getElementById('container')
-);
+// ReactDOM.render(
+//     <HashRouter path="/" component={Main}  path="app">
+//       <div>
+//           <Route exact path="/" component={Home}/>
+//           <Route exact path="/cars" component={Car}/>
+//           <Route exact path="/about" component={About}/>
+//       </div>
+//     </HashRouter>,
+//     document.getElementById('container')
+// );
+
+<Router>
+    <div>
+      <Route path="/" component={Main} history={browserHistory}>
+          <IndexRoute component={Home} />
+          <Route path="/cars" component={Car}/>
+          <Route path="/about" component={About}/>
+      </Route>
+    </div>
+</Router>
